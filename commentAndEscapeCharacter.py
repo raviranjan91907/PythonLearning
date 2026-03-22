351f78baa7e1694c12a66a1dd04783c752849212
raviranjan91907 <raviranjan91907@gmail.com>
2026-03-22 23:10:48 +0530

    starting Python learning

---
+++ commentAndEscapeCharacter.py
@@ -0,0 +1,16 @@
+
+# This is single line comment
+
+"""
+    this act as multi-line comment,
+    but it is a string
+    act as multi-line comment
+    until it assign
+"""
+
+#Escape Character
+print("Hello\nworld")
+print("Hello \"AJ\" how are you?")
+print("this will \t print tab")
+print("this will \a create a beep sound")
+
