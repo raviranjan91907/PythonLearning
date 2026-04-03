4e8ef9a95374c46444b13119b2849a85a66952a5
raviranjan91907 <raviranjan91907@gmail.com>
2026-04-03 22:42:11 +0530

    Lambda function learning

--- LambdaFunctions.py
+++ LambdaFunctions.py
@@ -1,3 +1,25 @@
 a=[[1,14],[5,6],[8,23]]
 a.sort(key=lambda x:x[1])
 print(a)
+
+
+nums = [1, 2, 3, 4]
+result = list(map(lambda x: x * 2, nums))
+print(result)
+
+
+nums = [1, 2, 3, 4, 5]
+result = list(filter(lambda x: x % 2 == 0, nums))
+print(result)
+
+
+from functools import reduce
+nums = [1, 2, 3, 4]
+result = reduce(lambda x, y: x + y, nums)
+print(result)
+
+
+def apply_func(x, func):
+    return func(x)
+
+print(apply_func(5, lambda x: x * x))
